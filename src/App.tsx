073a6b02d50cb06a6fdd1b724679c700daabc3a2
ryna <irina.glonyagina@gmail.com>
2023-08-10 02:03:41 +0300

    fix max time work

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React, {useEffect, useMemo, useState} from 'react';
+import React, {useMemo} from 'react';
 import './App.css';
 import {getStaticProps} from "./api/timeIntervalApi";
 import {formatData, freeInterval, getIntervals, makeTimes} from "./utils/timeInterval";
-import {Interval} from "./interface/interface";
 
 function App() {
     const busyTime = getStaticProps().busy;
 
-    const workInterval = useMemo(() => {
+    const workIntervals = useMemo(() => {
         const intervals = getIntervals(busyTime);
         const intervalsDate = intervals.map((item) => (formatData(item)));
         const freeTime = intervalsDate.map((item) => {
             const time = makeTimes(item, 30);
             return freeInterval(time)
         })
-        const freeIntervals = freeTime.reduce((a, e) => a.concat(e))
-        return freeIntervals
+        return freeTime.reduce((a, e) => a.concat(e))
     }, [busyTime])
 
     return (
         <div className="App">
-            {workInterval.map((item, index) =>
+            {workIntervals.map((item, index) =>
                 <div key={index}><p>{`${item.start}-${item.stop}`}</p></div>)}
         </div>
     );
